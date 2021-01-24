e63f4849662a2db6467f08ceeb6ceec8baf075a9
Jose Anasco <joseanasco1@gmail.com>
2021-01-24 01:22:21 +0100

    starting dsl creation

--- lib/supersonic/dll.rb
+++ lib/supersonic/dll.rb
@@ -1,61 +1,96 @@
 require 'ffi'
 
 module Supersonic
-  module DLL
-    lib_path = File.expand_path(__dir__ << "/../../ext")
+  module Sunvox
+    module DLL
+      lib_path = File.expand_path(__dir__ << "/../../ext")
 
-    extend FFI::Library
+      extend FFI::Library
 
-    ffi_lib ["#{lib_path}/sunvox.dylib"]
+      ffi_lib ["#{lib_path}/sunvox.dylib"]
 
-    attach_function :sv_init, [ :string, :int, :int, :uint32 ], :int
-    attach_function :sv_deinit, [], :int
-    attach_function :sv_open_slot, [ :int ], :int
-    attach_function :sv_lock_slot, [ :int ], :int
-    attach_function :sv_unlock_slot, [ :int ], :int
-    attach_function :sv_close_slot, [ :int ], :int
-    attach_function :sv_new_module, [ :int, :string, :string, :int, :int, :int ], :int
-    attach_function :sv_connect_module, [ :int, :int, :int ], :int
-    attach_function :sv_send_event, [ :int, :int, :int, :int, :int, :int, :int ], :int
+      attach_function :sv_init, [ :string, :int, :int, :uint32 ], :int
+      attach_function :sv_deinit, [], :int
+      attach_function :sv_open_slot, [ :int ], :int
+      attach_function :sv_lock_slot, [ :int ], :int
+      attach_function :sv_unlock_slot, [ :int ], :int
+      attach_function :sv_close_slot, [ :int ], :int
+      attach_function :sv_new_module, [ :int, :string, :string, :int, :int, :int ], :int
+      attach_function :sv_connect_module, [ :int, :int, :int ], :int
+      attach_function :sv_send_event, [ :int, :int, :int, :int, :int, :int, :int ], :int
+    end
 
-    alias_method :init, :sv_init
-    alias_method :deinit, :sv_deinit
-    alias_method :open_slot, :sv_open_slot
-    alias_method :lock_slot, :sv_lock_slot
-    alias_method :unlock_slot, :sv_unlock_slot
-    alias_method :close_slot, :sv_close_slot
-    alias_method :new_module, :sv_new_module
-    alias_method :connect_module, :sv_connect_module
-    alias_method :send_event, :sv_send_event
+    class Slot
+      include Supersonic::Sunvox::DLL
+
+      def initialize(slot_number, &block)
+        @slot_number = slot_number
+        open(&block)
+      end
+
+      def new_module(*args)
+        lock { sv_new_module @slot_number, *args }
+      end
+
+      def connect_module(*args)
+        lock { sv_connect_module @slot_number, *args }
+      end
+
+      def send_event(*args)
+        sv_send_event @slot_number, *args
+      end
+
+      private
+
+      def open(&block)
+        sv_open_slot @slot_number
+        block.call self
+        sv_close_slot @slot_number
+      end
+
+      def lock(&block)
+        sv_lock_slot @slot_number
+        block_return_value = block.call
+        sv_unlock_slot @slot_number
+        block_return_value
+      end
+    end
+
+    module DSL
+      include Supersonic::Sunvox::DLL
+
+      def with_sunvox_init(&block)
+        version = sv_init nil, 44100, 2, 0
+        p "version: #{version}"
+        block.call
+        sv_deinit
+      end
+
+      def with_slot(slot_number, &block)
+        Supersonic::Sunvox::Slot.new(slot_number) do |slot|
+          slot.instance_eval(&block)
+        end
+      end
+    end
   end
 end
 
-extend Supersonic::DLL
+extend Supersonic::Sunvox::DSL
 
 def play_me
-  version = init nil, 44100, 2, 0
-  p "version: #{version}"
+  with_sunvox_init do
+    with_slot 0 do
+      mod_num = new_module "Generator", "Generator", 0, 0, 0
 
-  open_slot 0
+      if mod_num >= 0
+        connect_module mod_num, 0
 
-  lock_slot 0
-  mod_num = new_module 0, "Generator", "Generator", 0, 0, 0
-  unlock_slot 0
+        send_event 0, 64, 128, mod_num + 1, 0, 0
 
-  if mod_num >= 0
-    p "module number: #{mod_num}"
-
-    lock_slot 0
-    connect_module 0, mod_num, 0
-    unlock_slot 0
-
-    send_event 0, 0, 64, 128, mod_num + 1, 0, 0
-    sleep 1
-    # 128 = NOTE_OFF in third argument
-    send_event 0, 0, 128, 0, 0, 0, 0
+        sleep 1
+        # 128 = NOTE_OFF in second argument
+        send_event 0, 128, 0, 0, 0, 0
+      end
+    end
   end
-
-  close_slot 0
-  deinit
 end
-
